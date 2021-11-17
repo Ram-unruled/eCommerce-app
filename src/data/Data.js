0b7d0d0f47f6a8916a219cf27b8b6137bce1e684
Protosem_Team_06 <protosem06@forgeforward.local>
2021-11-17 09:55:33 +0530

    master

---
+++ src/data/Data.js
@@ -0,0 +1,103 @@
+const products = [
+    {
+        "id": 1,
+        "title": "Nike backpack",
+        "cate": "Backpack",
+        "price": 6000,
+        "image": "https://media.very.co.uk/i/very/RNA9Q_SQ1_0000000005_GREY_SLf/nike-elemental-backpack-grey.jpg?$180x240_retinamobilex2$",
+        "amount": 1
+    },
+    {   
+        "id": 2,
+        "title": "RGB multicolor mouse",
+        "cate": "Mouse",
+        "price": 3000,
+        "image": "https://5.imimg.com/data5/SELLER/Default/2021/6/EB/DX/EO/11998608/gaming-mouse-rgb-multi-colour-500x500.jpg",
+        "amount": 1
+    },
+    {
+        "id": 3,
+        "title": "Lazy Knit Slip On IDP sneakers For Men  (Black) ",
+        "cate": "Shoes",
+        "price": 4000,
+        "image": "https://m.media-amazon.com/images/I/61u-19CcpLL._UX500_.jpg",
+        "amount": 1
+    },
+    {
+        "id": 4,
+        "title": "Men Black Leather Wallet - Regular Size",
+        "cate": "Wallet",
+        "price": 2000,
+        "image": "https://staticimg.titan.co.in/Titan/Catalog/TW109LM1BK_2.jpg?pView=pdp",
+        "amount": 1
+    },
+    {
+        "id": 5,
+        "title": "UV Protection Aviator Sunglasses",
+        "cate": "glasses",
+        "price": 2500,
+        "image": "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Frukminim1.flixcart.com%2Fimage%2F880%2F1056%2Fjhjg13k0%2Fsunglass%2F7%2Fb%2Fc%2F55-0rb3025i088-3f55-ray-ban-original-imaf5jazzjktdnx2.jpeg%3Fq%3D50",
+        "amount": 1
+    },
+    {
+        "id": 6,
+        "title": "Modified McQueen",
+        "cate": "TOY",
+        "price": 2000,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7pfr6wrtkLR7xzNK9_CPxzAc8AHMOrdgILw&usqp=CAU",
+        "amount": 1
+    },
+    {
+        "id": 7,
+        "title": "Women Black Heels Sandal",
+        "cate": "Shoes",
+        "price": 5000,
+        "image": "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Frukminim1.flixcart.com%2Fimage%2F880%2F1056%2Fjylvde80%2Fsandal%2Fg%2F9%2Fz%2Ftriac-1-4-call-it-spring-black-original-imafgt5dnzwhrfjh.jpeg%3Fq%3D50",
+        "amount": 1
+    },
+    {
+        "id": 8,
+        "title": "LED wheel Skateboard",
+        "cate": "Skateboard",
+        "price": 7000,
+        "image": "https://www.verywellfit.com/thmb/n6-Yqxow5TFLY5I65cm3c_sZelM=/900x0/filters:no_upscale():max_bytes(150000):strip_icc()/71ux6N-PrL._AC_SL1500_-93b1f8da01d3410faef35ea8d221e033.jpg",
+        "amount": 1 
+    },
+    {
+        "id": 9,
+        "title": "HarryPotter Book Series",
+        "cate": "Book",
+        "price": 12000,
+        "image": "https://hips.hearstapps.com/sev.h-cdn.co/assets/16/25/980x490/landscape-1466693711-harry-potter-movie-icons-by-batnamz-d8js1bt.jpg?resize=1200:*",
+        "amount": 1
+    },
+    {
+        "id": 10,
+        "title": "HotWheels 20Car Gift Pack",
+        "cate": "TOY",
+        "price": 2000,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQET7AJaq93m41AuYMgGFFe8RG7soKQXwihbZCZZtKgfYAEVvL3UkAVw4-KWWhkQtEbxMI&usqp=CAU",
+        "amount": 1
+    },
+    {
+        "id": 11,
+        "title": "Titan Luxury edition v5 ",
+        "cate": "watch",
+        "price": 13000,
+        "image": "https://cdn.luxe.digital/media/2020/05/21140728/best-luxury-watch-brands-hublot-luxe-digital.jpg",
+        "amount": 1
+    },
+    {
+        "id": 12,
+        "title": "HRX Neoprene ",
+        "cate": "Dumbbell",
+        "price": 2000,
+        "image": "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Frukminim1.flixcart.com%2Fimage%2F416%2F416%2Fksgehzk0%2Fdumbbell%2Ft%2Fu%2Fk%2Fneoprene-coated-5-kg-x-12-hrx-original-imag6ymgwncehccf.jpeg%3Fq%3D70",
+        "amount": 1
+    }
+
+
+]
+
+
+export default products;
